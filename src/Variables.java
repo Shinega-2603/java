f1b61720134d579440d89de0109e5834af3a2ea4
shinega <shinegaperiasamy@gmail.com>
2021-04-14 22:32:06 +0530

    first commit by shinega

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Variables {
+    public static void main(String[] args){
+
+        int n=2;
+        for(int i=10;i>0;i--)
+        {
+            System.out.println(n+"*"+i+"="+n*i);
+        }
+
+    }
+}
