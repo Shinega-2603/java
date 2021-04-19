a30b4eaef97abc2b5d9f038c28a458b06e02ceef
shinega <shinegaperiasamy@gmail.com>
2021-04-19 23:52:39 +0530

    typecasting done.

---
+++ src/reverseascii.java
@@ -0,0 +1,11 @@
+public class reverseascii {
+    public static void main(String[] args) {
+        int b = 97;
+        char c;
+        c = (char) b;
+        System.out.println(c);
+
+        int d = c;
+        System.out.println(d);
+    }
+}
---
+++ src/typecasting.java
@@ -0,0 +1,9 @@
+
+
+public class typecasting {
+    public static void main(String[] args) {
+        String b = "12345g";
+        int a = Integer.parseInt(b);
+        System.out.println(a);
+    }
+}
