179a7af95d526b0e9c2af688e7da1c7a462ccd70
shinega <shinegaperiasamy@gmail.com>
2021-04-27 23:52:35 +0530

    done.

--- src/arrays.java
+++ src/arrays.java
@@ -18,6 +18,14 @@ public class arrays {
 //        for(boolean i:arr){
 //            System.out.println(i);
 //        }
+//        arrayAscending();
+//        arrayDescending();
+//        charaAscending();
+        charaDescending();
+
+    }
+
+    public static void arrayAscending() {
         int[] arr = {5, 3, 8, 1, 2};
         int temp = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -28,6 +36,61 @@ public class arrays {
                     arr[j] = temp;
                 }
             }
+        }
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i]);
+
+    }
+
+    public static void arrayDescending() {
+        int[] arr = {2, 5, 4, 9, 1};
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+        }
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i]);
+
+    }
+
+    public static void charaAscending() {
+        char[] arr = {'f', 'w', 'a', 'd', 'b'};
+        char temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+        }
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i]);
+
+    }
+
+    public static void charaDescending() {
+
+        char[] arr = {'l', 'd', 'r', 'i', 'z'};
+        char temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
         }
         for (int i = 0; i < arr.length; i++)
             System.out.println(arr[i]);
---
+++ src/homework2.java
@@ -0,0 +1,58 @@
+public class homework2 {
+    public static void main(String[] args) {
+//       pattern();
+//       odd();
+        calender();
+    }
+
+    public static void pattern() {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.println(i + "." + j);
+            }
+        }
+    }
+
+    public static void odd() {
+        String a = "shinega";
+        for (int i = 0; i < a.length(); i++) {
+            if (i % 2 != 0) {
+                System.out.println(a.charAt(i));
+            }
+        }
+    }
+
+    public static void calender() {
+        int year = 2020;
+        for (int month = 1; month <= 12; month++) {
+            for (int date = 1; date <= 31; date++) {
+                if (month % 2 != 0 && month != 7) {
+                    System.out.println(year + "-" + month + "-" + date);
+                }
+            }
+        }
+        for (int month = 1; month <= 7; month++) {
+            for (int date = 1; date <= 30; date++) {
+                if (month % 2 == 0 && month != 2) {
+                    System.out.println(year + "-" + month + "-" + date);
+                }
+            }
+        }
+        for (int month = 7; month <= 12; month++) {
+            for (int date = 1; date <= 31; date++) {
+                if (month == 7) {
+                    System.out.println(year + "-" + month + "-" + date);
+                } else if (month % 2 == 0) {
+                    System.out.println(year + "-" + month + "-" + date);
+                }
+            }
+        }
+        int month = 2;
+        if (month == 2) {
+            for (int date = 1; date <= 29; date++) {
+                System.out.println(year + "-" + month + "-" + date);
+            }
+        }
+
+    }
+}
