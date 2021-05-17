31de8ebfdf6ba91855e9d74d1c344bf523df1d0c
shinega <shinegaperiasamy@gmail.com>
2021-05-18 00:19:26 +0530

    done.

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -3,21 +3,16 @@
 public class SecondLargest {
     public static void main(String[] args) {
         int[] a = {5, 2, 10, 6, 8, 9};
-        int highest = a[0];
-        int secondHighest = a[1];
+        int first = a[0];
+        int second = a[0];
         for (int i = 0; i < a.length; i++) {
 
-            if (a[i] > highest) {
-
-
-                secondHighest = highest;
-
-
-                highest = a[i];
-            } else if (a[i] > secondHighest && a[i] != highest)
-
-                secondHighest = a[i];
+            if (a[i] > first) {
+                second = first;
+                first = a[i];
+            } else if (a[i] > second && a[i] < first)
+                second = a[i];
         }
-        System.out.println(secondHighest);
+        System.out.println(second);
     }
 }
---
+++ src/minArray.java
@@ -0,0 +1,15 @@
+public class minArray {
+    public static void main(String[] args) {
+        int n = 4;
+        int[] arr = {1, 3, 8, 3, 5};
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < n) {
+                int temp = n - arr[i];
+                arr[i] += temp;
+                count += temp;
+            }
+        }
+        System.out.println(count);
+    }
+}
